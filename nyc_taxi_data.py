8eebc65eb5b469cc51672f0f744c5304d09f3542
Camilo <camilo.santivanez@ug.uchile.cl>
2026-04-06 17:40:15 -0400

    feat(nyc_taxi_data.py): add click-based CLI and refactor notebook to script: - Implement Click for command-line argument parsing - Prepare script for docker automation - Add error handling for table and data ingestion

--- nyc_taxi_data.py
+++ nyc_taxi_data.py
@@ -1,81 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Ingest Data
-# First we import all the necessary libraries
-
-# In[1]:
-
-
 import pandas as pd 
 from sqlalchemy import create_engine, Integer, DateTime
+import click
 
-
-# Now we import the data into a dataframe
-
-# In[2]:
-
-
-# Parquet
+#NYC green taxi data
 taxi_trips_nov2025 = "https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2025-11.parquet"
-#CSV
 zone_dataset = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv"
 
+#LOAD DATA INTO DATAFRAMES 
 df_taxi_trips = pd.read_parquet(taxi_trips_nov2025)
 df_zone = pd.read_csv(zone_dataset)
 
-
-# # First visualization of the data that we are going to use:
-
-# In[3]:
-
-
-df_taxi_trips
-
-
-# In[4]:
-
-
-df_taxi_trips.dtypes
-
-
-# In[5]:
-
-
-df_zone
-
-
-# In[6]:
-
-
-df_zone.dtypes
-
-
-# # Preparing the data for the ingestion into Postgres
-
-# In[7]:
-
-
-# Creating an engine using SQLAlchemy
-engine = create_engine('postgresql+psycopg2://root:root@localhost:5432/green_ny_taxi')
-
-
-# Get DDL schema (sql command): 
-
-# In[8]:
-
-
-print(pd.io.sql.get_schema(df_taxi_trips, name="green_taxi_data", con=engine))
-
-
-# Check dtypes:
-
-# In[9]:
-
-
-#To deal with NaN values, Pandas considers columns type as Float instead of Integer
-#So we deal with it manually, ensuring that the data type in SQL is as we expected
-
+#Ensuring data types
 dtypes = { 
     "VendorID": Integer(),
     "lpep_pickup_datetime": DateTime(),
@@ -86,46 +24,33 @@ dtypes = {
     "trip_type": Integer()
 }   
 
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='green_ny_taxi', help='PostgreSQL database name')
+@click.option('--target-table', default='green_taxi_data', help='Target table name')
+def ingest_data(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table):
 
-# Checking again the DDL schema
+    # Creating an engine using SQLAlchemy
+    engine = create_engine(f'postgresql+psycopg2://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
-# In[10]:
+    # Create table first
+    try: 
+        print("Creating table ...")
+        df_taxi_trips.head(0).to_sql(name=f'{target_table}', con=engine, if_exists = "replace")
+    except Exception as e:
+        print(f'Error at creating the table: {e}')
+        return
+    #Load into Postgres
+    try:
+        print("Loading data into Postgres")
+        df_taxi_trips.to_sql(name=f'{target_table}', con=engine, if_exists = "append")
+    except Exception as e:
+        print(f'Error inserting data into the table: {e}')
+        return 
 
-
-print(pd.io.sql.get_schema(df_taxi_trips, name="green_taxi_data", con=engine, dtype=dtypes))
-
-
-# Now we check the schema structure is correctly created:
-
-# In[11]:
-
-
-df_taxi_trips.head(0).to_sql(name="green_taxi_data", con=engine, if_exists = "replace")
-# 0 means it works 
-
-
-# # Finally, we ingest the data into PostgresQL
-
-# In[12]:
-
-
-df_taxi_trips.to_sql(name="green_taxi_data", con=engine, if_exists = "append")
-
-
-# We check if everything works:
-
-# In[13]:
-
-
-check = pd.read_sql("SELECT * FROM green_taxi_data LIMIT 10;", con = engine)
-print(check)
-
-
-# Number of rows:
-
-# In[14]:
-
-
-rows_number = pd.read_sql("SELECT COUNT(1) FROM green_taxi_data;", con = engine)
-print(rows_number)
-
+# To make it executable
+if __name__ == '__main__':
+    ingest_data()
